e9277baf183cf2f989029bf9a7190ba6d1a4ffdf
Thaddeus Crews <repiteo@outlook.com>
2025-03-13 09:35:48 -0500

    Core: Integrate C++20 `[[likely]]`/`[[unlikely]]`

--- core/object/worker_thread_pool.cpp
+++ core/object/worker_thread_pool.cpp
@@ -189,7 +189,7 @@ void WorkerThreadPool::_thread_function(void *p_user) {
 			MutexLock lock(thread_data->pool->task_mutex);
 
 			bool exit = thread_data->pool->_handle_runlevel(thread_data, lock);
-			if (unlikely(exit)) {
+			if (exit) [[unlikely]] {
 				break;
 			}
 
@@ -277,7 +277,7 @@ void WorkerThreadPool::_notify_threads(const ThreadData *p_current_thread_data, 
 		if (th.current_task) {
 			// Good thread for promoting low-prio?
 			if (to_promote && th.awaited_task && th.current_task->low_priority) {
-				if (likely(&th != p_current_thread_data)) {
+				if (&th != p_current_thread_data) [[likely]] {
 					th.cond_var.notify_one();
 				}
 				th.signaled = true;
@@ -285,7 +285,7 @@ void WorkerThreadPool::_notify_threads(const ThreadData *p_current_thread_data, 
 			}
 		} else {
 			if (to_process) {
-				if (likely(&th != p_current_thread_data)) {
+				if (&th != p_current_thread_data) [[likely]] {
 					th.cond_var.notify_one();
 				}
 				th.signaled = true;
@@ -305,7 +305,7 @@ void WorkerThreadPool::_notify_threads(const ThreadData *p_current_thread_data, 
 			continue;
 		}
 		if (th.awaited_task) {
-			if (likely(&th != p_current_thread_data)) {
+			if (&th != p_current_thread_data) [[likely]] {
 				th.cond_var.notify_one();
 			}
 			th.signaled = true;
@@ -461,12 +461,12 @@ void WorkerThreadPool::_wait_collaboratively(ThreadData *p_caller_pool_thread, T
 			p_caller_pool_thread->signaled = false;
 
 			bool exit = _handle_runlevel(p_caller_pool_thread, lock);
-			if (unlikely(exit)) {
+			if (exit) [[unlikely]] {
 				break;
 			}
 
 			bool wait_is_over = false;
-			if (unlikely(p_task == ThreadData::YIELDING)) {
+			if (p_task == ThreadData::YIELDING) [[unlikely]] {
 				if (p_caller_pool_thread->yield_is_over) {
 					p_caller_pool_thread->yield_is_over = false;
 					wait_is_over = true;
